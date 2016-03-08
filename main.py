317d040268aa8b86eb68441a009149ecbd2abc38
Johan Wanglof <jwanglof@gmail.com>
2016-03-08 15:30:33 +0100

    Just a bunch of horsing around to try stuff

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
 import time
-from wiringx86 import GPIOGalileoGen2 as GPIO
 
-from utils import Temperature, Database
-from utils.pins import analogPins, digitalPins
-
-from utils.MagneticValve import MagneticValue
+from utils.AutomaticWateringSystem import AutomaticWateringSystem
 from utils.WateringQueue import WateringQueue
 
 # TODO
-# Kan ha en kö med vilka växter som ska vattnas. En växt vattnas åt gången. Då spelar huvudslangens diameter ingen roll!
 # Om det inte rinner något vatten ska summern låta (måste ha en Grove som kollar vattenflödet då!)
 # ?? Borde implementera någon kö som har alla sensorer i sig som kollar varje minut (?).
 #    Ska klasserna pusha in sig själva varje minut till en kö som väntar på att en klass blir klar, och sen kör nästa?
 
-gpio = GPIO(debug=True)
+# gpio = GPIO(debug=True)
 #
 # buzzer_pin = digitalPins.get(8)
 # gpio.pinMode(buzzer_pin, gpio.OUTPUT)
@@ -27,60 +22,67 @@ gpio = GPIO(debug=True)
 # gpio.pinMode(servo_pin, gpio.PWM)
 # gpio.setPWMPeriod(servo_pin, 1500)
 
+ValveQueue = WateringQueue(True)
+
 if __name__ == '__main__':
-    # NOTE: There will exist one class PER temperature sensor
-    temperature_pin = 0
-    GetTemperature = Temperature(analogPins.get(temperature_pin), 30)
-
-    # NOTE: There will exist one class PER magnetic valve
-    magnetic_valve_pin = 6
-    ChiliMagneticValve = MagneticValue(digitalPins.get(magnetic_valve_pin), 'CHILI #1', True)
-
-    buzzer_pin = 3
-    FlowerMagneticValve = MagneticValue(digitalPins.get(buzzer_pin), 'FLOWER #1', True)
-
-    ValveQueue = WateringQueue(True)
+    # # NOTE: There will exist one class PER temperature sensor
+    # temperature_pin = 0
+    # GetTemperature = Temperature(analogPins.get(temperature_pin), 30)
+    #
+    # # NOTE: There will exist one class PER magnetic valve
+    # magnetic_valve_pin = 6
+    # ChiliMagneticValve = MagneticValve(digitalPins.get(magnetic_valve_pin), 'CHILI #1', True)
+    #
+    # buzzer_pin = 3
+    # FlowerMagneticValve = MagneticValve(digitalPins.get(buzzer_pin), 'FLOWER #1', True)
+    #
 
     # database = Database()
+    AWS = AutomaticWateringSystem('LED', temperature_pin=0, magnetic_valve_pin=6, debug=True)
+    AWS2 = AutomaticWateringSystem('BUZZER', temperature_pin=0, magnetic_valve_pin=8, debug=True)
 
     try:
         while True:
+            print 'LOOOOP'
+            AWS.MagneticValve.send_open_valve_signal()
+            AWS2.MagneticValve.send_open_valve_signal()
             # temp = GetTemperature.get_celsius()
             # database.add_statistic(temperature=temp)
-            print 'Exceeded: ' + str(GetTemperature.has_exceeded_threshold())
-            print 'Temp: ' + str(GetTemperature.get_celsius())
-            print 'Added: ' + str(ChiliMagneticValve.is_added_to_queue)
-            print 'Added 2: ' + str(FlowerMagneticValve.is_added_to_queue)
+            # print 'Exceeded: ' + str(GetTemperature.has_exceeded_threshold())
+            # print 'Temp: ' + str(GetTemperature.get_celsius())
+            # print 'Added: ' + str(ChiliMagneticValve.is_added_to_queue)
+            # print 'Added 2: ' + str(FlowerMagneticValve.is_added_to_queue)
 
-            try:
-                if GetTemperature.has_exceeded_threshold() and not ChiliMagneticValve.is_added_to_queue:
-                    # gpio.digitalWrite(buzzerPin, gpio.HIGH)
-                    # gpio.digitalWrite(led_pin, gpio.HIGH)
-                    ValveQueue.add_valve(ChiliMagneticValve)
-                    ValveQueue.open_next()
-                elif not GetTemperature.has_exceeded_threshold() and ChiliMagneticValve.is_added_to_queue:
-                    # gpio.digitalWrite(buzzerPin, gpio.LOW)
-                    # gpio.digitalWrite(led_pin, gpio.LOW)
-                    ValveQueue.close_current()
-            except IndexError as e:
-                print e
+            # try:
+            #     if GetTemperature.has_exceeded_threshold() and not ChiliMagneticValve.is_added_to_queue:
+            #         # gpio.digitalWrite(buzzerPin, gpio.HIGH)
+            #         # gpio.digitalWrite(led_pin, gpio.HIGH)
+            #         ValveQueue.add_valve(ChiliMagneticValve)
+            #         ValveQueue.open_next()
+            #     elif not GetTemperature.has_exceeded_threshold() and ChiliMagneticValve.is_added_to_queue:
+            #         # gpio.digitalWrite(buzzerPin, gpio.LOW)
+            #         # gpio.digitalWrite(led_pin, gpio.LOW)
+            #         ValveQueue.close_current()
+            # except IndexError as e:
+            #     print e
+            #
+            # try:
+            #     if GetTemperature.has_exceeded_threshold() and not FlowerMagneticValve.is_added_to_queue:
+            #         # gpio.digitalWrite(buzzerPin, gpio.HIGH)
+            #         # gpio.digitalWrite(led_pin, gpio.HIGH)
+            #         ValveQueue.add_valve(FlowerMagneticValve)
+            #         ValveQueue.open_next()
+            #     elif not GetTemperature.has_exceeded_threshold() and FlowerMagneticValve.is_added_to_queue:
+            #         # gpio.digitalWrite(buzzerPin, gpio.LOW)
+            #         # gpio.digitalWrite(led_pin, gpio.LOW)
+            #         ValveQueue.close_current()
+            # except IndexError as e:
+            #     print e
 
-            try:
-                if GetTemperature.has_exceeded_threshold() and not FlowerMagneticValve.is_added_to_queue:
-                    # gpio.digitalWrite(buzzerPin, gpio.HIGH)
-                    # gpio.digitalWrite(led_pin, gpio.HIGH)
-                    ValveQueue.add_valve(FlowerMagneticValve)
-                    ValveQueue.open_next()
-                elif not GetTemperature.has_exceeded_threshold() and FlowerMagneticValve.is_added_to_queue:
-                    # gpio.digitalWrite(buzzerPin, gpio.LOW)
-                    # gpio.digitalWrite(led_pin, gpio.LOW)
-                    ValveQueue.close_current()
-            except IndexError as e:
-                print e
-
-            time.sleep(1)
+            time.sleep(2)
     except KeyboardInterrupt:
-        GetTemperature.gpio.cleanup()
+        # GetTemperature.gpio.cleanup()
+        print 'CLEEEEEANUP'
 
 
 # # pin = 13
